155e7f2c81ad574579aa48e3085e4d479cbf6f58
Austin <austinchristensen4@gmail.com>
2021-03-03 11:00:45 -0700

    Initial Commit

---
+++ SwiftUIListApp/SwiftUIListAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIListAppApp.swift
+//  SwiftUIListApp
+//
+//  Created by Austin Christensen on 3/3/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIListAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
